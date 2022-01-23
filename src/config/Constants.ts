b40e34fb4c6c6a798989a4a50ee45388229479fa
Luis Carlos <luisczdidi@gmail.com>
2022-01-23 02:02:25 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,6 +1,11 @@
 require('dotenv').config();
 
+let PORT;
+if (process.env.PORT) {
+	PORT = `:${process.env.PORT}`
+}
+
 export const config = {
-	API_URL: `${process.env.URL}:${process.env.PORT}`,
+	API_URL: `${process.env.URL}${PORT}`,
 	MONGO_CONNECTION:	process.env.MONGO_STRING_CONN,
 }
